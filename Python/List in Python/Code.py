14a5d77532d52b9db1022754eb6e5b9ebe03ec2c
Ubaid Ur Rehman <119524246+UBAID0315@users.noreply.github.com>
2024-01-07 16:03:15 +0500

    Update Code.py

--- Python/List in Python/Code.py
+++ Python/List in Python/Code.py
@@ -1,7 +1,1 @@
-employee_salary = [10000,20000,60000,80000,70000,100000,65000,75000]
 
-result = list(map(lambda salary: (salary*0.1)+salary,employee_salary))
-filtered = list(filter(lambda salary: salary < 50000,result))
-
-print(f'Employee Salary less than 50k: {filtered}')
-print(f'The budget: {sum(filtered)}')
